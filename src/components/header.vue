26f08fa62190b09b3181bb87394d6e29f2802b60
Bread-etc <1054115624@qq.com>
2024-05-06 18:32:08 +0800

    add alias in vite.config.ts and tsconfig.json

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-        header
+    <div class="flex sticky top-0">
+        <a href="#logo">
+            <img src="@images/bread.webp" alt="image/nav" class="w-24px h-24px" />
+        </a>
+        <div>Bread_etc Blog</div>
+        <div v-for="route in routes"></div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { ref, onMounted, onUnmounted } from 'vue'
 
+const router = useRouter();
+const routes = ref([
+    { path: "/", title: "首页" },
+    { path: "/essay", title: "随笔" },
+    { path: "/contact", title: "留言" },
+    { path: "/about", title: "关于" }
+])
 </script>
 
-<style module>
-
-</style>
+<style module></style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
+// 引入路径
+import { resolve } from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     UnoCSS(),
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "@components": resolve(__dirname, "src/components"),
+      "@images": resolve(__dirname, "src/assets/images")
+    }
+  }
 })
